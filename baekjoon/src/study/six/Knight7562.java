9fb2c2b7886e42aefad1ac65b6d3705099bc665f
jongseo park <sociojs85@gmail.com>
2023-03-20 22:02:11 +0900

    230320 home

--- baekjoon/src/study/six/Knight7562.java
+++ baekjoon/src/study/six/Knight7562.java
@@ -17,6 +17,7 @@ public class Knight7562 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		
 		while (T-- > 0) {
 			l = Integer.parseInt(br.readLine());
@@ -32,12 +33,15 @@ public class Knight7562 {
 			tarCoord[0] = Integer.parseInt(st.nextToken());
 			tarCoord[1] = Integer.parseInt(st.nextToken());
 			
-			System.out.println(bfs(curCoord));
-			
+			sb.append(bfs(curCoord) + "\n");
 		}
+		System.out.println(sb);
 	}
 	
 	static int bfs(int[] coord) {
+		if (coord[0] == tarCoord[0] && coord[1] == tarCoord[1])
+			return 0;
+		
 		Queue<int[]> q = new ArrayDeque<>();
 		boolean[][] visited = new boolean[l][l];
 		
@@ -47,22 +51,23 @@ public class Knight7562 {
 		int cnt = 0;
 		
 		while (!(q.isEmpty())) {
-			int tr = coord[0];
-			int tc = coord[1];
-			
 			int size = q.size();
-			
 			cnt++;
-			
+
 			while (--size >= 0) {
+				int[] tmpCoord = q.poll();
+				
 				for (int i=0; i<8; i++) {
+					int tr = tmpCoord[0];
+					int tc = tmpCoord[1];
+					
 					tr += dr[i];
 					tc += dc[i];
 					
 					if (tr == tarCoord[0] && tc == tarCoord[1])
 						return cnt;
 					
-					if (tr >= 0 && tr < l && tc >= 0 && tc < l) {
+					if (tr >= 0 && tr < l && tc >= 0 && tc < l && !visited[tr][tc]) {
 						q.offer(new int[] {tr, tc});
 						visited[tr][tc] = true;
 					}				
@@ -70,7 +75,6 @@ public class Knight7562 {
 			}
 			
 		}
-		
 		
 		return 0;
 	}
