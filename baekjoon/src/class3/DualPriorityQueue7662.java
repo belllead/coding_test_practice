6cf22700da1b7c3379b1af40ee1f146f8d9ef657
jongseopark <sociojs85+dev@gmail.com>
2023-03-21 18:02:48 +0900

    230321 class

---
+++ baekjoon/src/class3/DualPriorityQueue7662.java
@@ -0,0 +1,17 @@
+package class3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DualPriorityQueue7662 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		while (T-- > 0) {
+			int N = Integer.parseInt(br.readLine());
+		}
+	}
+}
